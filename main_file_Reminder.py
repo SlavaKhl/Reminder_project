a33ccac2b98499402926305a402fdf85d7c4d2c2
SlavaKhl <hvv1953@gmail.com>
2024-11-24 16:27:17 +0200

    Создание графического интерфейса через tkinter и его настройка...

--- main_file_Reminder.py
+++ main_file_Reminder.py
@@ -1,5 +1,18 @@
 from tkinter import *
 from tkinter import messagebox as mb
-import pygame
+#import pygame
 import datetime
 import time
+
+window = Tk()
+window.title("Напоминалка")
+window.geometry("400x250")
+
+e = Entry(window, font=("Arial", 20))
+e.pack(pady=20)
+
+btn = Button(window, font=("Arial", 20), text="установить напоминание")
+btn.pack()
+
+window.mainloop()
+
