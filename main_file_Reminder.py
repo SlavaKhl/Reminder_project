6ec05d9eeb94b866cfc95ce4240918e1077a49cc
SlavaKhl <hvv1953@gmail.com>
2024-11-25 13:32:07 +0300

    Добавил функцию "set_reminder" для устанвовки времени напоминания.

--- main_file_Reminder.py
+++ main_file_Reminder.py
@@ -5,19 +5,35 @@ from tkinter import messagebox as mb
 import datetime
 import time
 
+def set_reminder():
+    rem_time = e.get()
+    if rem_time:
+        t_m.config(text="")
+        hour, minute = rem_time.split(":")
+        full_now_time = datetime.datetime.now()
+        remind_time = full_now_time.replace(hour=int(hour), minute=int(minute))
+        t_m.config(text=f"Напоминание установлено на {hour}:{minute}")
+        t_m.config(fg="green")
+    else:
+        t_m.config(text="Поле не заполнено", font=("Comic Sans MS", 16))
+        t_m.config(fg="red")
+
 window = Tk()
 window.title("Напоминание")
 window.geometry("400x250")
 window.iconbitmap(default="alarm_reminder.ico")
 
-e = Entry(window, font=("Comic Sans MS", 20))
-e.pack(pady=20)
+l_e = Label(window, text="Укажите время для напоминания:", font=("Comic Sans MS", 16))
+l_e.pack()
 
-btn = Button(window, font=("Comic Sans MS", 20), text="Установить напоминание")
-btn.pack()
+e = Entry(window, font=("Comic Sans MS", 20))
+e.pack()
+
+btn = Button(window, font=("Comic Sans MS", 20), text="Установить напоминание", command=set_reminder)
+btn.pack(pady=20)
 
 t_m = Label(window, font=("Comic Sands MS", 20))
-t_m.pack(pady=20)
+t_m.pack()
 
 window.mainloop()
 
