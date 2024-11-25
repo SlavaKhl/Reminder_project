405b33ff4bce5a3c4a9aa4ca7a651698cf5dc5f3
SlavaKhl <hvv1953@gmail.com>
2024-11-25 15:38:06 +0300

    Добавил функцию установки времени и напоминания.

--- new_Reminder.py
+++ new_Reminder.py
@@ -6,6 +6,24 @@ import pygame
 import time
 
 
+def set():
+    global t
+    rem = sd.askstring("Время напоминания", "Введите время в формате ЧЧ:ММ (24-часовой формат)")
+    if rem:
+        try:
+            hour = int(rem.split(":")[0])
+            minute = int(rem.split(":")[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+            label.config(text=f"Напоминание установлено на: {hour:02}:{minute:02}")
+        except ValueError:
+            mb.showerror("Ошибка", "Неверный формат времени")
+
+
 window = Tk()
 window.title("Напоминание")
 label = Label(text="Установите напоминание", font=("Arial", 14))
