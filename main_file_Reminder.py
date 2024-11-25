cb677b0b48305d7086abcba4b19c55e3bdce30ff
SlavaKhl <hvv1953@gmail.com>
2024-11-25 14:49:29 +0300

    Добавлена функция "play_music" и аудио файл

--- main_file_Reminder.py
+++ main_file_Reminder.py
@@ -1,22 +1,42 @@
 from email.policy import default
 from tkinter import *
 from tkinter import messagebox as mb
-#import pygame
+import pygame
 import datetime
 import time
 
+
 def set_reminder():
+    global remind_time_second
     rem_time = e.get()
     if rem_time:
         t_m.config(text="")
         hour, minute = rem_time.split(":")
         full_now_time = datetime.datetime.now()
-        remind_time = full_now_time.replace(hour=int(hour), minute=int(minute))
+        remind_time_second = full_now_time.replace(hour=int(hour), minute=int(minute)).timestamp()
         t_m.config(text=f"Напоминание установлено на {hour}:{minute}")
         t_m.config(fg="green")
+        check_now_time()
     else:
         t_m.config(text="Поле не заполнено", font=("Comic Sans MS", 16))
         t_m.config(fg="red")
+
+
+def check_now_time():
+    global remind_time_second
+    now_time_sec = time.time()
+    if now_time_sec >= remind_time_second:
+        play_music()
+        remind_time_second = None
+        return
+    window.after(1000, check_now_time)
+
+def play_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load("5836_pod-zvonok.ru__.mp3")
+    pygame.mixer.music.play("")
+
+remind_time_second = None
 
 window = Tk()
 window.title("Напоминание")
